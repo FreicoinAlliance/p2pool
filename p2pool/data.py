2732785dd18b87aff3a47ab96d2a67a2242f06e8
Rikski <hendrik-richter@freenet.de>
2014-12-29 18:01:52 +0100

    Added tx_version in def get_share

--- p2pool/data.py
+++ p2pool/data.py
@@ -243,7 +243,7 @@ class Share(object):
                 share_info=share_info,
                 ref_merkle_link=dict(branch=[], index=0),
                 last_txout_nonce=last_txout_nonce,
-                hash_link=prefix_to_hash_link(bitcoin_data.tx_type.pack(gentx)[:-32-8-4], cls.gentx_before_refhash),
+                hash_link=prefix_to_hash_link(bitcoin_data.tx_type.pack(gentx)[:-32-8-4-(tx_version in set([2]) and 4 or 0)], cls.gentx_before_refhash), # for FRC Parent added -(tx_version in set([2]) and 4 or 0)
                 merkle_link=bitcoin_data.calculate_merkle_link([None] + other_transaction_hashes, 0),
             ))
             assert share.header == header # checks merkle_root
