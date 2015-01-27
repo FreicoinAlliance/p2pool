ed9bf0f08af8cffef7283e147f5ffc73e456e1c4
Rikski <hendrik-richter@freenet.de>
2015-01-27 04:17:53 +0100

    Stop support for BTC II
    Remove the set for a fixed type of arguments (1) for the new transactions "gentx".

--- p2pool/bitcoin/data.py
+++ p2pool/bitcoin/data.py
@@ -106,7 +106,7 @@ tx_type = pack.VersionedType([
         ('script', pack.VarStrType()),
     ]))),
     ('lock_time', pack.IntType(32)),
-    ('refheight', pack.IntType(32), set([2]), 0),
+    ('refheight', pack.IntType(32)),
 ])
 
 merkle_link_type = pack.ComposedType([
